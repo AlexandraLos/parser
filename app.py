4f29b57d2336e30f297186e39cfc8659ec1d2cba
Aliaksandra Los <aliaksandra_los@epam.com>
2021-03-20 20:02:02 +0300

    Final

---
+++ app.py
@@ -0,0 +1,28 @@
+from pars import Parser
+
+
+def main():
+    vacancy_title = str(input('Введите название вакнасии: '))
+    words_in = str(input('Введите ключевые слова через пробел: ')).split(' ')
+    jobs = Parser(vacancy_title)
+    info = jobs.vacancies_pars(words_in)
+    # print(info[0])
+    # print(info[1])
+
+    for i in info[0]:
+        for j in i:
+            print(f'На странице №{int(j[0]) + 1} найдено {i[j]} ключевых слов')
+    for i in info[1]:
+        for j in i:
+            words = i[j]
+            for w in words:
+                if int(words[w]) == 0 or w == 'all_vacancies':
+                    continue
+                else:
+                    all_vacancies = int(words['all_vacancies'])
+                    d = round((int(words[w]) / all_vacancies), 3)
+                    print(f'В среднем на странице №{int(j[0]) + 1} слово "{w}" встречается {d} раз')
+
+
+if __name__ == '__main__':
+    main()
---
+++ pars.py
@@ -0,0 +1,68 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+
+class Parser:
+    URL = 'https://rabota.by/search/vacancy?clusters=true&text={}&area=1002'
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/87.0.4280.141 Safari/537.36 OPR/73.0.3856.415', 'accept': '*/*'}
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_info_vacancies(self, link):
+        print(link)
+        response = get(link, headers=self.HEADERS)
+        soup = BeautifulSoup(response.text, 'html.parser').find_all('div', {'class': 'vacancy-description'})
+        description_vacancies = ""
+        for s in soup:
+            description_vacancies += s.get_text('\n').lower()
+        return description_vacancies
+
+    def get_current_vacancy(self, html, words_in):
+        link = html.find('a').get('href')
+        des_job = self.get_info_vacancies(link)
+        k = 0
+        w = {}
+        for i in words_in:
+            w[i] = 0
+        for i in des_job.split(' '):
+            word = i.replace('\n', '').strip().replace('.', '').replace(',', '').replace(':', '')
+            if word in words_in:
+                k += 1
+            for h in w:
+                if h == word:
+                    w[h] += 1
+        return k, w
+
+    def vacancies_pars(self, words_in):
+        jobs = []
+        w = {}
+        for i in words_in:
+            w[i] = 0
+        words = []
+        for page in range(0, 5):
+            print(f'Парсинг страницы №{page + 1}')
+            g = self.URL.format(self.url)
+            response = get(f'{g}&page={page}', headers=self.HEADERS)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            results = soup.find_all('div', {'class': 'vacancy-serp-item'})
+            count = 0
+            all_words_page = {}
+            for i in words_in:
+                all_words_page[i] = 0
+            for result in results:
+                try:
+                    job = self.get_current_vacancy(result, words_in)
+                    count += job[0]
+                    j = job[1]
+                    for step_word in j:
+                        for b in all_words_page:
+                            if b == step_word:
+                                all_words_page[b] += j[step_word]
+                    all_words_page['all_vacancies'] = len(results)
+                except Exception as e:
+                    print(e)
+            words.append({str(page): all_words_page})
+            jobs.append({str(page): count})
+        return jobs, words
