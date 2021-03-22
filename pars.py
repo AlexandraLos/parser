39bae3c6b36983f643937ed53f09e023ef4dfc9f
AlexandraLos <59026817+AlexandraLos@users.noreply.github.com>
2021-03-22 16:38:57 +0300

    changed header

--- pars.py
+++ pars.py
@@ -4,8 +4,7 @@ from bs4 import BeautifulSoup
 
 class Parser:
     URL = 'https://rabota.by/search/vacancy?clusters=true&text={}&area=1002'
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                             'Chrome/87.0.4280.141 Safari/537.36 OPR/73.0.3856.415', 'accept': '*/*'}
+    HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
     def __init__(self, name_vac):
         self.name_vac = name_vac
